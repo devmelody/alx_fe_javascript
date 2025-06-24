163bbff8fb0d43a02ae4cc0e829f795be2e6e5a2
Mel <melodynk01@gmail.com>
2025-06-24 14:36:19 +0100

    first commit

---
+++ dom-manipulation/script.js
@@ -0,0 +1,38 @@
+const quoteDisplay = document.getElementById('quoteDisplay');
+const newQuoteBtn = document.getElementById('newQuote').addEventListener('click', showRandomQuote);
+const newQuoteText = document.getElementById("newQuoteText");
+const newQuoteCategory = document.getElementById("newQuoteCategory");
+
+const quotes = [
+  { text: "Be yourself; everyone else is already taken.", category: "Inspiration" },
+  { text: "Simplicity is the ultimate sophistication.", category: "Wisdom" },
+  { text: "Stay hungry, stay foolish.", category: "Motivation" }
+];
+
+function showRandomQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const quoteToBeDisplayed = quotes[randomIndex]
+  quoteDisplay.textContent = `"${quoteToBeDisplayed.text}" - ${quoteToBeDisplayed.category}`;
+}
+
+function createAddQuoteForm() {
+  const newQuote = newQuoteText.value;
+  const newCategory = newQuoteCategory.value;
+  const newestQuote = `"${newQuote}" - ${newCategory}` ;
+
+  if (newQuote !== '' && newCategory !== '') {
+
+    quotes.push({text: newQuote, category: newCategory});
+
+    quoteDisplay.textContent = newestQuote;
+
+        // Clear input fields after showing
+    newQuoteText.value = '';
+    newQuoteCategory.value = '';
+  } else {
+    alert("Please fill in both the quote and category.");
+  }
+
+console.log(quotes);
+
+}
